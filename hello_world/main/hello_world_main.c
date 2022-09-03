6d7da31f76121adcc7163426ef3d0d9748d5739d
sandy-zgf <2657434027@qq.com>
2022-09-03 16:22:42 +0800

    task finished

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -13,7 +13,16 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
-#include "freertos/queue.h" //add by zgf
+#include "freertos/timers.h" //add by zgf
+
+void TimerCallback(TimerHandle_t xTimer)
+{
+    const char * name;
+
+    name = pcTimerGetName(xTimer);
+
+    printf("one shot!!! %s\n",name);
+}
 
 typedef struct A_STRUCT
 {
@@ -25,115 +34,33 @@ char *pcText = "Hello World zgf";
 
 aStruct aStructTest = {6, 9};
 
-void sendTask(void *pvParam)
-{
-
-    QueueHandle_t QHandle;
-
-    QHandle = (QueueHandle_t)pvParam;
-
-    BaseType_t xStatus;
-
-    char *pStrToSend;
-
-    int i = 0;
-
-    while (1)
-    {
-
-        pStrToSend = (char *)malloc(50);
-
-        snprintf(pStrToSend, 50, "strInglessthan50 %d", i);
-        i++;
-
-        xStatus = xQueueSend(QHandle,&pStrToSend, 0);
-
-        if (xStatus == pdPASS)
-            printf("send successfully\n");
-        else
-            printf("send failed\n");
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
-void recTask(void *pvParam)
-{
-
-    QueueHandle_t QHandle;
-
-    QHandle = (QueueHandle_t)pvParam;
-
-    char *pStrToReceive;
-
-    BaseType_t xStatus;
-
-
-    while (1)
-    {
-
-        if (uxQueueMessagesWaiting(QHandle) != 0)
-        {
-            xStatus = xQueueReceive(QHandle,&pStrToReceive, 0);
-
-            if (xStatus == pdPASS)
-                printf("receive successfully \n%s\n",pStrToReceive);
-            else
-                printf("receive failed\n");
-            free(pStrToReceive);
-        }
-        else
-        {
-            printf("no data received !\n");
-        }
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
 int testNum = 99;
 int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 void app_main(void)
 {
-    QueueHandle_t QHandle;
 
-    QHandle = xQueueCreate(5, sizeof(char *));
+    TimerHandle_t xTimer1;
+    TimerHandle_t xTimer2;
 
-    if (QHandle != NULL)
-    {
-        printf(" create queue handle successfully\n");
+    xTimer1 = xTimerCreate("Timer1", pdMS_TO_TICKS(1000), pdTRUE, (void *)0, TimerCallback); // run once per second
+    xTimer2 = xTimerCreate("Timer2", pdMS_TO_TICKS(2000), pdTRUE, (void *)0, TimerCallback); // run once per second
 
-        // TaskHandle_t pxTask = NULL;
-        xTaskCreate(sendTask, "sendTask", 1024 * 5, (void *)QHandle, 1, NULL);
-        xTaskCreate(recTask, "rectask", 1024 * 5, (void *)QHandle, 1, NULL);
-    }
-    else
-    {
-        printf(" error!!!\n");
-    }
+    xTimerStart(xTimer1, 0);
+    xTimerStart(xTimer2, 0);
 
-    // UBaseType_t iStack;
+    vTaskDelay(pdMS_TO_TICKS(6000));
+    xTimerChangePeriod(xTimer1,pdMS_TO_TICKS(6000), 0); //
 
     // while (1)
     // {
-    //     iStack = uxTaskGetStackHighWaterMark(myHandle); //获取当前堆栈的值
-    //     printf("the number of istask is %d\n",iStack);
-    //     vTaskDelay(1000 / portTICK_PERIOD_MS); //delay
-
+    //     vTaskDelay(pdMS_TO_TICKS(1000));
+    //     xTimerReset(xTimer2, 0);
     // }
+    
 
-    // vTaskSuspend(myHandle);
+    // vTaskDelay(pdMS_TO_TICKS(6000));
 
-    // vTaskDelay(5000 / portTICK_PERIOD_MS);
-    // vTaskResume(myHandle); //恢复当前的任务；
 
-    // vTaskDelay(3000/ portTICK_PERIOD_MS);
-    // vTaskSuspend(myHandle);
-
-    // vTaskPrioritySet(myHandle,3);
-
-    // iPriority = uxTaskPriorityGet(myHandle);
-
-    // printf("iPriority: %d\n", iPriority);
+    // xTimerStop(xTimer1,0);
 }
